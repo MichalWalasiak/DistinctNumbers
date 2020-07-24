776e6ca4313ab895420e235ff926f2799d303184
Michał Walasiak <m.walasiak@gmail.com>
2020-07-24 12:50:06 +0200

    Make method non static

--- src/main/java/Algorythms/SortService.java
+++ src/main/java/Algorythms/SortService.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class SortService {
 
-    public static List<Integer> sortUnique(List<Integer> input) {
+    public List<Integer> sortUnique(List<Integer> input) {
         return input.stream().sorted().distinct().collect(Collectors.toList());
     }
 }

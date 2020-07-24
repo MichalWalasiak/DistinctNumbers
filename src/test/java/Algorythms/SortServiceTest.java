1509b95948e7b9ef416175ff2886dc52b3cab6a4
Michał Walasiak <m.walasiak@gmail.com>
2020-07-24 12:50:23 +0200

    Create test class

---
+++ src/test/java/Algorythms/SortServiceTest.java
@@ -0,0 +1,31 @@
+package Algorythms;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortServiceTest {
+
+    @Test
+    @DisplayName("should return list that size is equal to number of distinct")
+    void sortUnique_returns_listWithDistinctNumbers() {
+        // given
+        SortService sortService = new SortService();
+        List<Integer> integer = new ArrayList<>();
+        integer.add(4);
+        integer.add(1);
+        integer.add(2);
+        integer.add(4);
+        integer.add(3);
+
+        // when
+        integer = sortService.sortUnique(integer);
+
+        // then
+        assertEquals(integer.size(), 4);
+    }
+}
